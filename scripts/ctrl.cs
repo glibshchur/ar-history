99454ea15a53e3cb79548b4d8a1eb19225ab9b43
MrTaras123 <55591217+MrTaras123@users.noreply.github.com>
2020-12-12 01:19:06 +0200

    Add files via upload

---
+++ scripts/ctrl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ctrl : MonoBehaviour
+{
+    public GameObject dino;
+    public GameObject spawner;
+    public GameObject go;
+    public Button button;
+    public Button end;
+    public Text text;
+    public void Start_game()
+    {
+
+        Destroy(GameObject.FindGameObjectWithTag("player"));
+        Destroy(GameObject.FindGameObjectWithTag("spawner"));
+        Destroy(GameObject.FindGameObjectWithTag("game_over"));
+        Instantiate(dino , dino.transform.position,Quaternion.identity);
+        Instantiate(spawner, spawner.transform.position, Quaternion.identity);
+        Instantiate(go, go.transform.position, Quaternion.identity);
+    }
+    private void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("player") != null)
+        {
+            button.interactable = false;
+            end.interactable = true;
+        }
+        else
+        {
+            end.interactable = false;
+            button.interactable = true;
+        }
+    }
+    public void End_game()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("player"));
+        Destroy(GameObject.FindGameObjectWithTag("spawner"));
+        Destroy(GameObject.FindGameObjectWithTag("game_over"));
+        GameObject[] obs = GameObject.FindGameObjectsWithTag("obstacle");
+        foreach (var a in obs)
+        {
+            Destroy(a);
+        }
+    }
+}
---
+++ scripts/spawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    public move_obstacle[] low;
+    public move_obstacle[] high;
+    public float cd;
+    public float spd;
+    bool can_spawn=true;
+    void FixedUpdate()
+    {
+        if (can_spawn)
+        {
+            int a = Random.Range(0,2);
+            if (a % 2 == 0)
+            {
+                int b = Random.Range(0,low.Length);
+                low[b].GetComponent<move_obstacle>().change(spd); 
+                Instantiate(low[b], low[b].transform.position,Quaternion.identity);
+            }
+            else
+            {
+                int b = Random.Range(0, high.Length);
+                 high[b].GetComponent<move_obstacle>().change(spd);
+                Instantiate(high[b], high[b].transform.position, Quaternion.identity);
+            }
+            can_spawn = false;
+            StartCoroutine(cool());
+        }
+    }
+    IEnumerator cool()
+    {
+        yield return new WaitForSeconds(cd);
+        can_spawn = true;
+    }
+}
